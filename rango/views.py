fcea9271466be1d28cc9dd5f1cfdeff5f85e7423
asimonia <alex.simonian@gmail.com>
2017-01-07 15:19:31 -0600

    Templates and Models

--- rango/views.py
+++ rango/views.py
@@ -4,10 +4,12 @@ from django.http import HttpResponse
 def index(request):
 	# Construct a dictionary to pass to the template engine as its context.
 	# Note the key boldmessage is the same as {{ boldmessage }}
-	context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+	context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!',
+					'ballmessage': 'Sweaty, beautiful, sweet balls'}
 
 	# Return a rendered response to send to the client
 	return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-	return HttpResponse("Rango says here is the about page")
+	context_dict = {'aboutmessage': 'This page is brought to you by Alex'}
+	return render(request, 'rango/about.html', context=context_dict)
