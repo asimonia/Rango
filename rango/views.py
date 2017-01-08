dcbebad208f3793832adf2269cfd8730992a3c96
asimonia <alex.simonian@gmail.com>
2017-01-07 18:34:07 -0600

    MTV

--- rango/views.py
+++ rango/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from rango.models import Category, Page
+
 def index(request):
 	# Construct a dictionary to pass to the template engine as its context.
 	# Note the key boldmessage is the same as {{ boldmessage }}
-	context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!',
-					'ballmessage': 'Sweaty, beautiful, sweet balls'}
+	category_list = Category.objects.order_by('-likes')[:5]
+	context_dict = {'categories': category_list}
 
 	# Return a rendered response to send to the client
 	return render(request, 'rango/index.html', context=context_dict)
@@ -13,3 +15,18 @@ def index(request):
 def about(request):
 	context_dict = {'aboutmessage': 'This page is brought to you by Alex'}
 	return render(request, 'rango/about.html', context=context_dict)
+
+def show_category(request, category_name_slug):
+	context_dict = {}
+
+	try:
+		category = Category.objects.get(slug=category_name_slug)
+		pages = Page.objects.filter(category=category)
+		context_dict['pages'] = pages
+		context_dict['category'] = category
+	except Category.DoesNotExist:
+		context_dict['category'] = None
+		context_dict['pages'] = None
+
+	return render(request, 'rango/category.html', context_dict)
+
